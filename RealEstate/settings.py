d5846f2c6f38190e88a99b1652a8eaf905bcb79d
Ashish anand <50195305+Ashish8135@users.noreply.github.com>
2020-07-15 02:04:02 +0530

    modify middleware

--- RealEstate/settings.py
+++ RealEstate/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -147,7 +148,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_DIRS = os.path.join(BASE_DIR, 'static')
+STATIC_DIRS = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # ADMIN_MEDIA_PREFIX = '/static/admin/'
