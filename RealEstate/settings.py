476aa97a573db79d9c79363b7540d82d5c8a43d9
Ashish anand <50195305+Ashish8135@users.noreply.github.com>
2020-07-15 02:49:27 +0530

    added collect-static file

--- RealEstate/settings.py
+++ RealEstate/settings.py
@@ -152,7 +152,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_DIRS = os.path.join(BASE_DIR, 'static')
+STATIC_DIRS = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
