1a5b38d04f3c21e1ff522caf44e670d730a3285d
Ashish anand <50195305+Ashish8135@users.noreply.github.com>
2020-07-15 03:11:04 +0530

    added collect-stat

--- RealEstate/settings.py
+++ RealEstate/settings.py
@@ -151,18 +151,18 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_DIRS = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_DIRS = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # ADMIN_MEDIA_PREFIX = '/static/admin/'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR,'RealEstate/static/')
-# ]
 STATICFILES_DIRS = [
-    STATIC_DIRS
+    os.path.join(BASE_DIR,'RealEstate/static/')
 ]
+# STATICFILES_DIRS = [
+#     STATIC_DIRS
+# ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
