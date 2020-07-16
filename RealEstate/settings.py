2e42f705c48f9a8bf75271c484ce27f96f160439
Ashish anand <50195305+Ashish8135@users.noreply.github.com>
2020-07-16 22:16:57 +0530

    debug changed

--- RealEstate/settings.py
+++ RealEstate/settings.py
@@ -189,7 +189,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'Anitajoshi4846@gmail.com'
-EMAIL_HOST_PASSWORD = 'Anitajoshi@12345'
+EMAIL_HOST_PASSWORD =  os.environ.get("EMAIL_HOST_PASSWORD")
 EMAIL_PORT = 587
 
 django_heroku.settings(locals())
