2b01e9a3121d1624dac8f0e808e764eebcbc685f
Ashish anand <50195305+Ashish8135@users.noreply.github.com>
2020-07-16 19:42:58 +0530

    email changed

--- RealEstate/settings.py
+++ RealEstate/settings.py
@@ -177,7 +177,7 @@ MESSAGE_TAGS = {
 }
 
 # Email configuration
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # EMAIL_HOST = 'smtp.gmail.com'
 # EMAIL_PORT=587
 # EMAIL_HOST_USER='anitajoshi4846@gmail.com'
